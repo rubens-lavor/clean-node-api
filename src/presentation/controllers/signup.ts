f5ad9ef50c87a5b2f48e610e4a128178e2eae0fc
rubens-lavor <haryson07@gmail.com>
2021-02-12 18:24:49 -0300

    chore: upgrade funtion ESLint

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,7 +3,7 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 
-export class SignUpController implements Controller{
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
