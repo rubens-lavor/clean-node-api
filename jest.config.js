9bb4766b35f6d75fdfb4f8d8274f908df39f9fbe
rubens-lavor <haryson07@gmail.com>
2021-02-21 22:30:50 -0300

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    //'!<rootDir>/src/main/**'
+    '<rootDir>/src/**/*.ts'
+    // '!<rootDir>/src/main/**'
   ],
   coverageDirectory: 'coverage',
-  //coverageProvider: 'babel',
+  // coverageProvider: 'babel',
   testEnvironment: 'node',
-  //preset: '@shelf/jest-mongodb',
+  // preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
