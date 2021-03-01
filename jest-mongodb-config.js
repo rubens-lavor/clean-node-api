40946058a1e4861b5a4ac1569a2757935f4c0f4e
rubens-lavor <haryson07@gmail.com>
2021-03-01 20:39:02 -0300

    chore: add jest configs for mongodb

---
+++ jest-mongodb-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mongobdMemoryServerOptions: {
+    instance: {
+      dbName: 'jest'
+    },
+    binary: {
+      version: '4.0.3',
+      skipMD5: true
+    },
+    autoStart: false
+  }
+}
