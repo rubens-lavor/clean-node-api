32367338967ef7010acee287a24ff66429c79641
rubens-lavor <haryson07@gmail.com>
2021-03-01 21:28:35 -0300

    feat: upgrade mongodbMemoryServerOptions

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  mongobdMemoryServerOptions: {
-    instance: {
-      dbName: 'jest'
-    },
+  mongodbMemoryServerOptions: {
     binary: {
       version: '4.0.3',
       skipMD5: true
     },
-    autoStart: false
+    autoStart: false,
+    instance: {}
   }
 }
