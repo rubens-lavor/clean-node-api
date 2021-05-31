bc646e8bcb533ed6ba85361b6aec890c9018453a
rubens-lavor <haryson07@gmail.com>
2021-05-31 19:55:36 -0300

    feat: ensure SignUpController calls Validation with correct value

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,4 +1,5 @@
 export * from '../../protocols'
 export * from '../../protocols/email-validator'
+export * from '../../helpers/validator/validation'
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
