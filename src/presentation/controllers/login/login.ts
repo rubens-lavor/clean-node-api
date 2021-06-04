0dea0e8647603dbf1b34a4b939d451e2c1e8fd75
rubens-lavor <haryson07@gmail.com>
2021-06-04 15:02:15 -0300

    refactor: change Authentication to receive a model

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -19,7 +19,11 @@ export class LoginController implements Controller {
 
       const { email, password } = httpRequest.body
 
-      const acessToken = await this.authentication.auth(email, password)
+      const acessToken = await this.authentication.auth({
+        email,
+        password
+      })
+
       if (!acessToken) {
         return unauthorized()
       }
