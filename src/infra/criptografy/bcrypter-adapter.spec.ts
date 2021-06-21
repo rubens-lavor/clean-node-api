54c6a2399e060b7414d6b0e1cd719799140ac779
rubens-lavor <haryson07@gmail.com>
2021-06-21 17:24:14 -0300

    test: ensure BcrypterAdapter returns false when compare fails

--- src/infra/criptografy/bcrypter-adapter.spec.ts
+++ src/infra/criptografy/bcrypter-adapter.spec.ts
@@ -6,8 +6,8 @@ jest.mock('bcrypt', () => ({
     return await new Promise(resolve => resolve('hash'))
   },
 
-  async compare (): Promise<string> {
-    return await new Promise(resolve => resolve('hash'))
+  async compare (): Promise<boolean> {
+    return await new Promise(resolve => resolve(true))
   }
 }))
 
@@ -47,9 +47,16 @@ describe('Bcrypter Adapter', () => {
     expect(compareSpy).toHaveBeenLastCalledWith('any_value', 'any_hash')
   })
 
-  test('Should return true when compare success', async () => {
+  test('Should return true when compare succeeds', async () => {
     const sut = makeSut()
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(true)
+  })
+
+  test('Should return false when compare fails', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(new Promise(resolve => (resolve(false))))
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(false)
   })
 })
