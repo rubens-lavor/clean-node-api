ed059195b2a6f4ca1cea758b600474e50e205e62
rubens-lavor <haryson07@gmail.com>
2021-06-22 21:45:05 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Shold return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'haryon07@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
